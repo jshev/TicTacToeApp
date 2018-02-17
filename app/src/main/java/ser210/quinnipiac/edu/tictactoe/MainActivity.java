c809edbda013e29e26975bed63f063ccb3c8aa7f
validName <JJShevchenko@quinnipiac.edu>
2018-02-16 20:28:03 -0500

    final commit

--- app/src/main/java/ser210/quinnipiac/edu/tictactoe/MainActivity.java
+++ app/src/main/java/ser210/quinnipiac/edu/tictactoe/MainActivity.java
@@ -1,11 +1,10 @@
 package ser210.quinnipiac.edu.tictactoe;
 
 import android.content.Intent;
-import android.media.Image;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -21,17 +20,9 @@ public class MainActivity extends AppCompatActivity {
     ImageButton butt6;
     ImageButton butt7;
     ImageButton butt8;
-    char turn = TTT2.HUMAN_PLAYER;
+    Button restart;
     int win = 0;
-
-
-    boolean isPressedPlayer = false;
-    boolean isPressedComputer = false;
-    ImageButton[] buttons = {butt0, butt1, butt2, butt3, butt4, butt5, butt6, butt7, butt8};
-
-    int clicks;
-    int lastClicks;
-
+    int[] buttonIds = new int[9];
     public static TicTacToe TTT2 = new TicTacToe();
 
     @Override
@@ -39,298 +30,126 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // gets welcome text from Login Activity
         Intent intent = getIntent();
         String welcomeText = intent.getStringExtra(EXTRA_WELCOME);
         TextView welcomeView = (TextView) findViewById(R.id.welcome);
         welcomeView.setText(welcomeText);
 
+        // set array of imagebuttons
+        buttonIds[0] = R.id.button0;
+        buttonIds[1] = R.id.button1;
+        buttonIds[2] = R.id.button2;
+        buttonIds[3] = R.id.button3;
+        buttonIds[4] = R.id.button4;
+        buttonIds[5] = R.id.button5;
+        buttonIds[6] = R.id.button6;
+        buttonIds[7] = R.id.button7;
+        buttonIds[8] = R.id.button8;
+
+        // sets up all buttons and makes simple tags
         butt0 = (ImageButton) findViewById(R.id.button0);
         butt0.setOnClickListener(imageButtonHandler);
         butt0.setTag(Integer.valueOf(0));
 
         butt1 = (ImageButton) findViewById(R.id.button1);
         butt1.setOnClickListener(imageButtonHandler);
-        butt0.setTag(Integer.valueOf(1));
+        butt1.setTag(Integer.valueOf(1));
 
         butt2 = (ImageButton) findViewById(R.id.button2);
         butt2.setOnClickListener(imageButtonHandler);
-        butt0.setTag(Integer.valueOf(2));
+        butt2.setTag(Integer.valueOf(2));
 
         butt3 = (ImageButton) findViewById(R.id.button3);
         butt3.setOnClickListener(imageButtonHandler);
-        butt0.setTag(Integer.valueOf(3));
+        butt3.setTag(Integer.valueOf(3));
 
         butt4 = (ImageButton) findViewById(R.id.button4);
         butt4.setOnClickListener(imageButtonHandler);
-        butt0.setTag(Integer.valueOf(4));
+        butt4.setTag(Integer.valueOf(4));
 
         butt5 = (ImageButton) findViewById(R.id.button5);
         butt5.setOnClickListener(imageButtonHandler);
-        butt0.setTag(Integer.valueOf(5));
+        butt5.setTag(Integer.valueOf(5));
 
         butt6 = (ImageButton) findViewById(R.id.button6);
         butt6.setOnClickListener(imageButtonHandler);
-        butt0.setTag(Integer.valueOf(6));
+        butt6.setTag(Integer.valueOf(6));
 
         butt7 = (ImageButton) findViewById(R.id.button7);
         butt7.setOnClickListener(imageButtonHandler);
-        butt0.setTag(Integer.valueOf(7));
+        butt7.setTag(Integer.valueOf(7));
 
         butt8 = (ImageButton) findViewById(R.id.button8);
         butt8.setOnClickListener(imageButtonHandler);
-        butt0.setTag(Integer.valueOf(8));
+        butt8.setTag(Integer.valueOf(8));
 
-        playGame();
+        // restart button is visible but cannot be clicked
+        restart = (Button) findViewById(R.id.resetbutton);
+        restart.setEnabled(false);
+
     }
 
     View.OnClickListener imageButtonHandler = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             ImageButton butt = (ImageButton) view;
-            // TextView winView = (TextView) findViewById(R.id.whoWon);
+            TextView winView = (TextView) findViewById(R.id.whoWon);
 
-            switch (butt.getId()) {
-                case R.id.button0:
-                    // butt.setImageResource(R.drawable.cross);
-                    setImageButton(0, isPressedPlayer);
-                    TTT2.setMove(TTT2.HUMAN_PLAYER, 0);
-                    turn = TTT2.COMPUTER_PLAYER;
-                    break;
-                case R.id.button1:
-                    // butt.setImageResource(R.drawable.cross);
-                    TTT2.setMove(TTT2.HUMAN_PLAYER, 1);
-                    setImageButton(1, isPressedPlayer);
-                    turn = TTT2.COMPUTER_PLAYER;
-                    break;
-                case R.id.button2:
-                    // butt.setImageResource(R.drawable.cross);
-                    TTT2.setMove(TTT2.HUMAN_PLAYER, 2);
-                    setImageButton(2, isPressedPlayer);
-                    turn = TTT2.COMPUTER_PLAYER;
-                    break;
-                case R.id.button3:
-                    // butt.setImageResource(R.drawable.cross);
-                    TTT2.setMove(TTT2.HUMAN_PLAYER, 3);
-                    setImageButton(3, isPressedPlayer);
-                    turn = TTT2.COMPUTER_PLAYER;
-                    break;
-                case R.id.button4:
-                    // butt.setImageResource(R.drawable.cross);
-                    TTT2.setMove(TTT2.HUMAN_PLAYER, 4);
-                    setImageButton(4, isPressedPlayer);
-                    turn = TTT2.COMPUTER_PLAYER;
-                    break;
-                case R.id.button5:
-                    // butt.setImageResource(R.drawable.cross);
-                    TTT2.setMove(TTT2.HUMAN_PLAYER, 5);
-                    setImageButton(5, isPressedPlayer);
-                    turn = TTT2.COMPUTER_PLAYER;
-                    break;
-                case R.id.button6:
-                    // butt.setImageResource(R.drawable.cross);
-                    TTT2.setMove(TTT2.HUMAN_PLAYER, 6);
-                    setImageButton(6, isPressedPlayer);
-                    turn = TTT2.COMPUTER_PLAYER;
-                    break;
-                case R.id.button7:
-                    // butt.setImageResource(R.drawable.cross);
-                    TTT2.setMove(TTT2.HUMAN_PLAYER, 7);
-                    setImageButton(7, isPressedPlayer);
-                    turn = TTT2.COMPUTER_PLAYER;
-                    break;
-                case R.id.button8:
-                    // butt.setImageResource(R.drawable.cross);
-                    TTT2.setMove(TTT2.HUMAN_PLAYER, 8);
-                    setImageButton(8, isPressedPlayer);
-                    turn = TTT2.COMPUTER_PLAYER;
-                    break;
-            }
+            // gets the button player clicked and sets move both in console and board
+            // then disables that button
+            int tagValue = (Integer) butt.getTag();
+            TTT2.setMove(TTT2.HUMAN_PLAYER, tagValue);
+            butt.setImageResource(R.drawable.cross);
+            butt.setEnabled(false);
 
-            // playGame(view);
+            // gets the move computer made and sets it on the board
+            int move = TTT2.getComputerMove();
+            TTT2.setMove(TTT2.COMPUTER_PLAYER, move);
+            setMoveOnBoard(move);
 
-            /*while (win == 0) {
-                if (turn == TTT2.COMPUTER_PLAYER) {
-                    TTT2.setMove(TTT2.COMPUTER_PLAYER, TTT2.getComputerMove());
-                    int tagValue = (Integer) butt.getTag();
-                    if (tagValue == TTT2.getComputerMove()) {
-                        butt.setImageResource(R.drawable.nought);
-                    }
-                    turn = TTT2.HUMAN_PLAYER;
-                }
-                win = TTT2.checkForWinner();
-            }
+            // check for winner from console version
+            win = TTT2.checkForWinner();
 
-            if (TTT2.checkForWinner()==2) {
+            // informs player who won by changing textview
+            // restart button can now be clicked
+            if (TTT2.checkForWinner() == 2) {
                 String winText = "'X' won! Play again?";
                 winView.setText(winText);
-            } else if (TTT2.checkForWinner()==3) {
+                restart.setEnabled(true);
+            } else if (TTT2.checkForWinner() == 3) {
                 String winText2 = "'O' won! Play again?";
                 winView.setText(winText2);
-            } else if (TTT2.checkForWinner()==1) {
+                restart.setEnabled(true);
+            } else if (TTT2.checkForWinner() == 1) {
                 String winText3 = "It's a TIE! Play again?";
                 winView.setText(winText3);
-            }*/
+                restart.setEnabled(true);
+            }
 
         }
+
     };
 
-    public void playGame() {
-        //ImageButton butt = (ImageButton) view;
-        final TextView winView = (TextView) findViewById(R.id.whoWon);
-        final Handler handler = new Handler();
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                while (win == 0) {
-                    if (turn == TTT2.HUMAN_PLAYER) {
-                        // probably the missing link...
-                    } else {
-                        TTT2.setMove(TTT2.COMPUTER_PLAYER, TTT2.getComputerMove());
-                /*int tagValue = (Integer) butt.getTag();
-                if (tagValue == TTT2.getComputerMove()) {
-                    butt.setImageResource(R.drawable.nought);
-                } */
-                        setImageButton(TTT2.getComputerMove(), isPressedComputer);
-                        turn = TTT2.HUMAN_PLAYER;
-                    }
-                    win = TTT2.checkForWinner();
-                }
-
-                if (TTT2.checkForWinner() == 2) {
-                    String winText = "'X' won! Play again?";
-                    winView.setText(winText);
-                } else if (TTT2.checkForWinner() == 3) {
-                    String winText2 = "'O' won! Play again?";
-                    winView.setText(winText2);
-                } else if (TTT2.checkForWinner() == 1) {
-                    String winText3 = "It's a TIE! Play again?";
-                    winView.setText(winText3);
-                }
-            }
-        });
-
-        /*while (win == 0) {
-            if (turn == TTT2.HUMAN_PLAYER) {
-                // probably the missing link...
-            } else {
-                TTT2.setMove(TTT2.COMPUTER_PLAYER, TTT2.getComputerMove());
-                *//*int tagValue = (Integer) butt.getTag();
-                if (tagValue == TTT2.getComputerMove()) {
-                    butt.setImageResource(R.drawable.nought);
-                } *//*
-                setImageButton(TTT2.getComputerMove(), isPressedComputer);
-                turn = TTT2.HUMAN_PLAYER;
-            }
-            win = TTT2.checkForWinner();
+    public void onClickRestart(View view) {
+        // restarts game by resetting everything
+        TextView winView = (TextView) findViewById(R.id.whoWon);
+        win = 0;
+        TTT2.clearBoard();
+        for (int i = 0; i < buttonIds.length; i++) {
+            ImageButton button = (ImageButton) findViewById(buttonIds[i]);
+            button.setImageResource(R.drawable.empty);
+            button.setEnabled(true);
         }
-
-        if (TTT2.checkForWinner()==2) {
-            String winText = "'X' won! Play again?";
-            winView.setText(winText);
-        } else if (TTT2.checkForWinner()==3) {
-            String winText2 = "'O' won! Play again?";
-            winView.setText(winText2);
-        } else if (TTT2.checkForWinner()==1) {
-            String winText3 = "It's a TIE! Play again?";
-            winView.setText(winText3);
-        }*/
+        winView.setText("");
+        restart.setEnabled(false);
     }
 
-    public void setImageButton(int whichButt, boolean isPressed) {
-
-        /*ImageButton butt = (ImageButton) view;
-        for each (butt in buttons) {
-            if (isPressedComputer) {
-                butt.setImageResource(R.drawable.nought);
-            } else if (isPressedPLayer) {
-                butt.setImageResource(R.drawable.cross);
-            } else {
-                butt.setImageResource(R.drawable.empty);
-            }
-        }*/
-
-        switch (whichButt) {
-            case 0:
-                if (isPressed == isPressedComputer) {
-                    butt0.setImageResource(R.drawable.nought);
-                } else if (isPressed == isPressedPlayer) {
-                    butt0.setImageResource(R.drawable.cross);
-                } else {
-                    butt0.setImageResource(R.drawable.empty);
-                }
-                break;
-            case 1:
-                if (isPressed == isPressedComputer) {
-                    butt1.setImageResource(R.drawable.nought);
-                } else if (isPressed == isPressedPlayer) {
-                    butt1.setImageResource(R.drawable.cross);
-                } else {
-                    butt1.setImageResource(R.drawable.empty);
-                }
-                break;
-            case 2:
-                if (isPressed == isPressedComputer) {
-                    butt2.setImageResource(R.drawable.nought);
-                } else if (isPressed == isPressedPlayer) {
-                    butt2.setImageResource(R.drawable.cross);
-                } else {
-                    butt2.setImageResource(R.drawable.empty);
-                }
-                break;
-            case 3:
-                if (isPressed == isPressedComputer) {
-                    butt3.setImageResource(R.drawable.nought);
-                } else if (isPressed == isPressedPlayer) {
-                    butt3.setImageResource(R.drawable.cross);
-                } else {
-                    butt3.setImageResource(R.drawable.empty);
-                }
-                break;
-            case 4:
-                if (isPressed == isPressedComputer) {
-                    butt4.setImageResource(R.drawable.nought);
-                } else if (isPressed == isPressedPlayer) {
-                    butt4.setImageResource(R.drawable.cross);
-                } else {
-                    butt4.setImageResource(R.drawable.empty);
-                }
-                break;
-            case 5:
-                if (isPressed == isPressedComputer) {
-                    butt5.setImageResource(R.drawable.nought);
-                } else if (isPressed == isPressedPlayer) {
-                    butt5.setImageResource(R.drawable.cross);
-                } else {
-                    butt5.setImageResource(R.drawable.empty);
-                }
-                break;
-            case 6:
-                if (isPressed == isPressedComputer) {
-                    butt6.setImageResource(R.drawable.nought);
-                } else if (isPressed == isPressedPlayer) {
-                    butt6.setImageResource(R.drawable.cross);
-                } else {
-                    butt6.setImageResource(R.drawable.empty);
-                }
-                break;
-            case 7:
-                if (isPressed == isPressedComputer) {
-                    butt7.setImageResource(R.drawable.nought);
-                } else if (isPressed == isPressedPlayer) {
-                    butt7.setImageResource(R.drawable.cross);
-                } else {
-                    butt7.setImageResource(R.drawable.empty);
-                }
-                break;
-            case 8:
-                if (isPressed == isPressedComputer) {
-                    butt8.setImageResource(R.drawable.nought);
-                } else if (isPressed == isPressedPlayer) {
-                    butt8.setImageResource(R.drawable.cross);
-                } else {
-                    butt8.setImageResource(R.drawable.empty);
-                }
-                break;
-        }
+    private void setMoveOnBoard(int move) {
+        // places computer's move on board and disables that button
+        ImageButton button = (ImageButton) findViewById(buttonIds[move]);
+        button.setImageResource(R.drawable.nought);
+        button.setEnabled(false);
     }
+
 }
